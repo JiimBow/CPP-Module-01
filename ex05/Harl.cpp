b44059b5f7e0b8a7b24390261de870b7a7fe6218
Jean-Baptiste Odone <jodone@2A4.42angouleme.fr>
2026-04-23 17:33:36 +0200

    ex 05

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 09:34:45 by jodone            #+#    #+#             */
-/*   Updated: 2026/04/23 11:24:08 by jodone           ###   ########.fr       */
+/*   Updated: 2026/04/23 15:02:48 by jodone           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ Harl::~Harl()
 
 void	Harl::debug()
 {
-	std::cout	<< "I love having extra bacon for my"
+	std::cout	<< "I love having extra bacon for my "
 				<< "7XL-double-cheese-triple-pickle-special-ketchup burger."
 				<< " I really do!\n";
 }
@@ -31,14 +31,14 @@ void	Harl::debug()
 void	Harl::info()
 {
 	std::cout	<< "I cannot believe adding extra bacon costs more money."
-				<< " You didn't put enough bacon in my burger! If you did,"
+				<< " You didn't put enough bacon in my burger!\nIf you did,"
 				<< " I wouldn't be asking for more!\n";
 }
 
 void	Harl::warning()
 {
 	std::cout	<< "I think I deserve to have some extra bacon for free."
-				<< " I've been coming for years, whereas you started "
+				<< " I've been coming for years, whereas you started\n"
 				<< "working here just last month.\n";
 }
 
@@ -49,14 +49,29 @@ void	Harl::error()
 
 void	Harl::complain(std::string level)
 {
+	typedef	void (Harl::*TableFunc)();
 	
-	switch (level)
+	struct 
 	{
-	case constant expression:
-		/* code */
-		break;
-	
-	default:
-		break;
+		std::string name;
+		TableFunc FuncPtr;
 	}
+	table[] =
+	{
+		{"debug", &Harl::debug},
+		{"info", &Harl::info},
+		{"warning",&Harl::warning},
+		{"error", &Harl::error}
+	};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (table[i].name == level)
+		{
+			(this->*table[i].FuncPtr)();
+			return ;
+		}
+	}
+	
+	std::cout << "Harl complains a lot, but that's incomprehensible\n";
 }
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,8 +6,28 @@
 /*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/23 09:34:37 by jodone            #+#    #+#             */
-/*   Updated: 2026/04/23 09:49:01 by jodone           ###   ########.fr       */
+/*   Updated: 2026/04/23 15:01:07 by jodone           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+#include <iostream>
+
+int	main(int ac, char** av)
+{
+	if (ac == 1)
+	{
+		std::cout << "You need to give one argument to Harl, he has to COMPLAIN !\n";
+		return 1;
+	}
+	if (ac > 2)
+	{
+		std::cout << "Harl got too much argument for complain\n";
+		return 1;
+	}
+
+	Harl harlington;
+	std::string	level = av[1];
+	
+	harlington.complain(level);
+}
